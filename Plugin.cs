40908b2623988573fd3857a1bb7ef727480c246f
Ventern <venternmodz@gmail.com>
2025-05-05 22:31:44 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace VenternPitgeos
+{
+    [BepInPlugin("Ventern.Pitgeos", "Ventern - Pitgeos", "0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Rect windowRect = new Rect(20, 20, 250, 150);
+        internal static bool showWindow = true;
+        internal static bool toggleValue = false;
+
+        void OnGUI()
+        {
+            if (showWindow)
+            {
+                windowRect = GUI.Window(0, windowRect, DrawWindow, "My Custom Window");
+            }
+        }
+
+        void DrawWindow(int windowID)
+        {
+            GUILayout.Label("Hello from OnGUI!");
+            toggleValue = GUILayout.Toggle(toggleValue, "Enable Something");
+
+            if (GUILayout.Button("Close Window"))
+            {
+                showWindow = false;
+            }
+
+            // Allow dragging the window
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
+        }
+    }
+}
