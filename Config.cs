7e999c57ffa5cf9e56c181d98e9f307f1efca5e2
Ventern <venternmodz@gmail.com>
2025-05-05 23:24:03 +0100

    version 0.1
    - added config system
    - made config load when start
    - made config save on setting change
    - added mod logic
    - added gui
    - reset defaults button
    - made F1 toggle GUI

---
+++ Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Numerics;
+
+namespace VenternPitgeos
+{
+    class Config
+    {
+        private static string MainFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ventern");
+        private static string SaveFilePath = Path.Combine(MainFolder, "Pitgeo-config.txt");
+
+        public static void SaveConfig()
+        {
+            if (File.Exists(SaveFilePath))
+            {
+                using StreamWriter streamWriter = new StreamWriter(SaveFilePath);
+                streamWriter.WriteLine($"pitgroundValue: {Plugin.pitgroundValue.ToString(CultureInfo.InvariantCulture)}");
+                streamWriter.WriteLine($"pitregularwallV2Value: {Plugin.pitregularwallV2Value.ToString(CultureInfo.InvariantCulture)}");
+                streamWriter.WriteLine($"pitlowerslipperywallValue: {Plugin.pitlowerslipperywallValue.ToString(CultureInfo.InvariantCulture)}");
+                streamWriter.WriteLine($"verticalwall1Value: {Plugin.verticalwall1Value.ToString(CultureInfo.InvariantCulture)}");
+                streamWriter.WriteLine($"verticalwall2Value: {Plugin.verticalwall2Value.ToString(CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                File.Create(SaveFilePath);
+                SaveConfig();
+            }
+        }
+
+        public static void LoadConfig()
+        {
+            if (File.Exists(SaveFilePath))
+            {
+                var data = File.ReadAllLines(SaveFilePath).Select(line => line.Split(':')).Where(parts => parts.Length == 2).ToDictionary(parts => parts[0], parts => parts[1]);
+                float.TryParse(data.GetValueOrDefault("pitgroundValue"), out Plugin.pitgroundValue);
+                float.TryParse(data.GetValueOrDefault("pitregularwallV2Value"), out Plugin.pitregularwallV2Value);
+                float.TryParse(data.GetValueOrDefault("pitlowerslipperywallValue"), out Plugin.pitlowerslipperywallValue);
+                float.TryParse(data.GetValueOrDefault("verticalwall1Value"), out Plugin.verticalwall1Value);
+                float.TryParse(data.GetValueOrDefault("verticalwall2Value"), out Plugin.verticalwall2Value);
+            }
+            else
+            {
+                File.Create(SaveFilePath);
+                LoadConfig();
+            }
+        }
+
+        private static void ParseVector(string value, out Vector3 result)
+        {
+            result = Vector3.Zero;
+            if (string.IsNullOrEmpty(value)) return;
+            var parts = value.Split(',');
+            if (parts.Length == 3 && float.TryParse(parts[0], out float x) && float.TryParse(parts[1], out float y) && float.TryParse(parts[2], out float z))
+                result = new Vector3(x, y, z);
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,4 @@
 ﻿using BepInEx;
-using System;
-using System.IO;
 using UnityEngine;
 
 namespace VenternPitgeos
@@ -8,30 +6,99 @@ namespace VenternPitgeos
     [BepInPlugin("Ventern.Pitgeos", "Ventern - Pitgeos", "0.1")]
     public class Plugin : BaseUnityPlugin
     {
-        internal static Rect windowRect = new Rect(20, 20, 250, 150);
+        internal static Rect windowRect = new Rect(20, 20, 220, 250);
         internal static bool showWindow = true;
-        internal static bool toggleValue = false;
+        internal static bool PitGeosEnabled;
+
+        internal GorillaSurfaceOverride pitground;
+        internal static float pitgroundValue = 1f;
+        internal GorillaSurfaceOverride pitregularwallV2;
+        internal static float pitregularwallV2Value = 1f;
+        internal GorillaSurfaceOverride pitlowerslipperywall;
+        internal static float pitlowerslipperywallValue = 1f;
+        internal GorillaSurfaceOverride verticalwall1;
+        internal static float verticalwall1Value = 1f;
+        internal GorillaSurfaceOverride verticalwall2;
+        internal static float verticalwall2Value = 1f;
+
+        void Start()
+        {
+            pitground = GameObject.Find("pit ground")?.GetComponent<GorillaSurfaceOverride>();
+            pitregularwallV2 = GameObject.Find("pitregularwallV2")?.GetComponent<GorillaSurfaceOverride>();
+            pitlowerslipperywall = GameObject.Find("pit lower slippery wall")?.GetComponent<GorillaSurfaceOverride>();
+            verticalwall1 = GameObject.Find("wallclimb/verticalwall (1)")?.GetComponent<GorillaSurfaceOverride>();
+            verticalwall2 = GameObject.Find("wallclimb/verticalwall")?.GetComponent<GorillaSurfaceOverride>();
+            VenternPitgeos.Config.LoadConfig();
+            VenternPitgeos.Config.SaveConfig();
+        }
 
         void OnGUI()
         {
             if (showWindow)
             {
-                windowRect = GUI.Window(0, windowRect, DrawWindow, "My Custom Window");
+                GUI.color = Color.red;
+                GUI.contentColor = Color.red;
+                GUI.backgroundColor = Color.black;
+                windowRect = GUI.Window(0, windowRect, DrawWindow, "Ventern Pitgeo's");
             }
         }
 
         void DrawWindow(int windowID)
         {
-            GUILayout.Label("Hello from OnGUI!");
-            toggleValue = GUILayout.Toggle(toggleValue, "Enable Something");
-
-            if (GUILayout.Button("Close Window"))
+            GUI.color = Color.red;
+            GUI.contentColor = Color.red;
+            GUI.backgroundColor = Color.red;
+            if (GUILayout.Button($"Reset to Default"))
             {
-                showWindow = false;
+                pitgroundValue = 1f;
+                pitregularwallV2Value = 1f;
+                pitlowerslipperywallValue = 1f;
+                verticalwall1Value = 1f;
+                verticalwall2Value = 1f;
+                ApplyMultiplier(pitground, 1f);
+                ApplyMultiplier(pitregularwallV2, 1f);
+                ApplyMultiplier(pitlowerslipperywall, 1f);
+                ApplyMultiplier(verticalwall1, 1f);
+                ApplyMultiplier(verticalwall2, 1f);
             }
-
-            // Allow dragging the window
+            pitgroundValue = Slider("Ground", pitgroundValue);
+            pitregularwallV2Value = Slider("Regular Wall ", pitregularwallV2Value);
+            pitlowerslipperywallValue = Slider("Slippery Wall", pitlowerslipperywallValue);
+            verticalwall1Value = Slider("Double Wall1", verticalwall1Value);
+            verticalwall2Value = Slider("Double Wall2", verticalwall2Value);
             GUI.DragWindow(new Rect(0, 0, 10000, 20));
+        }
+
+        void Update()
+        {
+            if (UnityInput.Current.GetKeyDown(KeyCode.F1))
+            {
+                showWindow = !showWindow;
+            }
+            ApplyMultiplier(pitground, pitgroundValue);
+            ApplyMultiplier(pitregularwallV2, pitregularwallV2Value);
+            ApplyMultiplier(pitlowerslipperywall, pitlowerslipperywallValue);
+            ApplyMultiplier(verticalwall1, verticalwall1Value);
+            ApplyMultiplier(verticalwall2, verticalwall2Value);
+        }
+
+        void ApplyMultiplier(GorillaSurfaceOverride surface, float value)
+        {
+            if (surface == null) return;
+            if (surface.extraVelMultiplier != value)
+                surface.extraVelMultiplier = value;
+            VenternPitgeos.Config.SaveConfig();
+            if (surface.extraVelMaxMultiplier != value)
+                surface.extraVelMaxMultiplier = value;
+            VenternPitgeos.Config.SaveConfig();
+        }
+
+        float Slider(string label, float value)
+        {
+            GUILayout.Label($"{label}: {value:F1}");
+            value = GUILayout.HorizontalSlider(value, 0f, 5f, GUILayout.Width(200));
+            value = Mathf.Round(value * 10f) / 10f;
+            return value;
         }
     }
 }
